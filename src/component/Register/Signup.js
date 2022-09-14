59cee64ed345b15304ff89bada58492bdf67374f
Utkp5 <utkp200@gmail.com>
2022-09-15 01:54:48 +0530

    pop up msg

--- src/component/Register/Signup.js
+++ src/component/Register/Signup.js
@@ -1,40 +1,70 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Signup.css";
+import axios from "axios";
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
 function Signup() {
+
+
+  const [userName,setuserName]= useState();
+  const [password,setpassword]= useState();
+  const [repeatPassword,setrepeatPassword]= useState();
+  const [userEmail,setuserEmail]= useState();
+
+
+const Handlesubmit = async () => {
+  const user = {
+    userName,
+    password,
+    repeatPassword,
+    userEmail,
+  };
+  await axios.post("http://localhost:5000/api/Signup",user).then(function (response) {
+    
+    if (response.data) {
+      Notify.success('Register Successfully')
+      window.location.href = "/Login";
+    }
+    
+
+  }).catch(function (error) {
+    Notify.failure('Something went Wrong!')
+    console.log(error);
+  });      
+}
+
+
   return (
     <div className="container">
       <label className="tab">Sign Up</label>
       <div className="login-form">
         <div className="sign-up-htm">
           <div className="group">
-            <label for="user" className="label">Username</label>
-            <input id="user" type="text" className="input" />
+            <label htmlFor="user" className="label">Username</label>
+            <input id="user" type="text" className="input" onChange={(e) => {
+              setuserName(e.target.value);
+            }}/>
           </div>
           <div className="group">
-            <label for="pass" className="label">Password</label>
-            <input
-              id="pass"
-              type="password"
-              className="input"
-              data-type="password"
-            />
+            <label htmlFor="pass" className="label">Password</label>
+            <input  type="password" className="input" onChange={(e) => {
+              setpassword(e.target.value);
+            }}/>
           </div>
           <div className="group">
-            <label for="pass" className="label">Repeat Password</label>
-            <input
-              id="pass"
-              type="password"
-              className="input"
-              data-type="password"
-            />
+            <label htmlFor="pass" className="label">Repeat Password</label>
+            <input  type="password" className="input"  onChange={(e) => {
+              setrepeatPassword(e.target.value);
+            }}/>
           </div>
           <div className="group">
-            <label for="pass" className="label">Email Address</label>
-            <input id="pass" type="text" className="input" />
+            <label htmlFor="pass" className="label">Email Address</label>
+            <input  type="text" className="input" onChange={(e) => {
+              setuserEmail(e.target.value);
+            }}/>
           </div>
           <div className="group">
-            <input type="submit" className="button" value="Sign Up" />
+            <input type="submit" className="button" value="Sign Up" onClick={() => {Handlesubmit();}}/>
           </div>
           <div className="hr"></div>
           <div className="foot-lnk">
