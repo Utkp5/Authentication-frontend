1d3d6511555d8bd6b328858b0617bdf35bf04290
Utkp5 <utkp200@gmail.com>
2022-09-14 19:44:03 +0530

    Login page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
 import Signup from './component/Register/Signup';
 import Frgtpassword from './component/forgotPassword/frgtPassword';
+import Login from './component/Login/Login';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Routes>
           <Route exact path='/' element={<Signup />} />
           <Route exact path='/Frgtpassword' element={<Frgtpassword />} />
+          <Route exact path='/Login' element={<Login />} />
         </Routes>
     </Router>
 
---
+++ src/component/Login/Login.js
@@ -0,0 +1,41 @@
+import React from 'react'
+import "./Login.css";
+
+function Login() {
+  return (
+    <div className="container">
+    <label className="tab">Sign In</label>
+    <div className="sign-in-htm">
+          <div className="group">
+            <label for="pass" className="label"> Email Address</label>
+            <input id="pass" type="text" className="input" />
+          </div>
+          <div className="group">
+            <label for="pass" className="label"> Password</label>
+            <input
+              id="pass"
+              type="password"
+              className="input"
+              data-type="password"
+            />
+          </div>
+          <div className="group_1">
+            <input id="check" type="checkbox" className="check" checked />
+            <label for="check"><span className="icon"></span> Keep me Signed in</label>
+          </div>
+          <div className="group">
+            <input type="submit" className="button" value="Sign In" />
+          </div>
+          <div className="hr"></div>
+		  <div className="foot-lnk foot_lnk1">
+		    <a href="/">Sign Up</a>
+		  </div>
+          <div className="foot-lnk">
+            <a href="/Frgtpassword">Forgot Password?</a>
+          </div>
+        </div>
+    </div>
+  )
+}
+
+export default Login
